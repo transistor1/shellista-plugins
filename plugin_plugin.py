f62e00773d576ddbfdf7cd72a813519443fcf1b2
Steve Russo <svrusso1@gmail.com>
2014-07-04 13:14:10 +0000

    Cleanup, comments

--- plugin_plugin.py
+++ plugin_plugin.py
@@ -9,6 +9,7 @@ import re
 import os
 import importlib
 import sys
+import contextlib
 
 from .. git import git_plugin as git
 from ... tools.toolbox import bash
@@ -17,6 +18,7 @@ alias=['plugins']
 shellista = sys.modules['__main__']
 shellista_dir = os.path.abspath(os.path.dirname(shellista.__file__))
 plugin_folder = os.path.join(shellista_dir,'plugins','extensions')
+plugins = None
 
 def _is_plugin_installed(module_name):
     #Quick-n-dirty hack to check which modules are installed.
@@ -26,7 +28,17 @@ def _is_plugin_installed(module_name):
        return True
     return False
 
+@contextlib.contextmanager
+def _context_chdir(new_path, create_path=False):
+    '''Change directories, saving the old path. To be used in
+         a with statement'''
+    os._old_path = os.getcwd()
+    os.chdir(new_path)
+    yield
+    os.chdir(os._old_path)
+
 class Plugin():
+    '''Represents a single plugin'''
     name = ''
     download_name = ''
     description = ''
@@ -38,10 +50,18 @@ class Plugin():
             setattr(self, kw, arg)
 
 class PluginFactory(list):
+    '''Abstract class for different plugin factories.
+        Purpose of this class is to convert text into plugin
+        information.
+        
+        Class was abstracted so we can use a different type
+        of update file if we outgrow pipe system'''
     def parse(self, line):
-        raise NotImplementedError()
+        raise NotImplementedError('Abstract method')
 
 class PipePluginFactory(PluginFactory):
+    '''Factory for converting pipe-delimited records into
+        Plugin()s'''
     def parse(self, line):
         items = string.split(line, '|')
         new_plugin = Plugin(name=items[0], download_name=items[1], description=items[2], git_url=items[3])
@@ -49,9 +69,13 @@ class PipePluginFactory(PluginFactory):
         return new_plugin
 
 class PluginFile(file):
+    '''File[-like] object representing a plugin file. Currently
+        derived directly from file as a placeholder in case
+        future functionality is required.'''
     pass
 
 class Plugins(list):
+    '''Represents a collection of Plugin objects.'''
     plugin_file = None
     plugin_factory = None
 
@@ -67,12 +91,6 @@ class Plugins(list):
             if not line.lstrip().startswith('#'):
                 plugin = self.plugin_factory.parse(line)
                 self.append(plugin)
-
-plugins = None
-with PluginFile(os.path.join(os.path.dirname(os.path.abspath(__file__))
-                    ,'plugin_urls.txt'),'r') as plugin_file:
-    plugins = Plugins(plugin_file, PipePluginFactory())
-    plugins.parse_file()
 
 def usage():
     print 'plugin [list [wildcard]|install <module name>|update <module name>]'
@@ -104,7 +122,8 @@ def plugin_install(self, plugin_name):
                         (path, ext) = os.path.splitext(path)
                         relpath = os.path.relpath(new_plugin_path, shellista_dir)
                         self._hook_plugin_main(relpath, path)
-                        
+                except Exception as e:
+                     raise e #Rethrow
                 finally:
                     os.chdir(cwd)
     else:
@@ -136,5 +155,11 @@ def main(self, line):
             print 'Error: {0}'.format(e)
     else:
         usage()
+
+#Enumerate all the available plugins on startup.
+with PluginFile(os.path.join(os.path.dirname(os.path.abspath(__file__))
+                    ,'plugin_urls.txt'),'r') as plugin_file:
+    plugins = Plugins(plugin_file, PipePluginFactory())
+    plugins.parse_file()
 
 
